397674fbc60ec6785658f33bcd7ce4241b24417c
Mariusz Nowak <medyk@medikoo.com>
2018-05-24 16:37:09 +0200

    custom: do not allow one line statements in brackets

--- src/_customizations/is-path-line-breaking.js
+++ src/_customizations/is-path-line-breaking.js
@@ -2,6 +2,12 @@
 
 const isNakedLineBreaking = require("./is-naked-line-breaking");
 
+const allowedStatements = new Set(["EmptyStatement"]);
 module.exports = function(path, printStatementSequence, options, print) {
+  const pathValue = path.getValue();
+  const type = pathValue && pathValue[0] && pathValue[0].type;
+  if (type && type.endsWith("Statement") && !allowedStatements.has(type)) {
+    return true;
+  }
   return isNakedLineBreaking(printStatementSequence(path, options, print));
 };
